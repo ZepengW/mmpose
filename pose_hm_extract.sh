47b706afee3caa2a64e9612bc65075d7d1957624
Zepeng-CK <37135654+oliverck@users.noreply.github.com>
2023-02-24 08:24:41 +0000

    update for yolox

--- pose_hm_extract.sh
+++ pose_hm_extract.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES='1' python demo/top_down_img_demo_with_mmdet_outhm.py \
     ../mmdetection/configs/yolox/yolox_x_8x8_300e_coco.py \
-    https://download.openmmlab.com/mmdetection/v2.0/yolo/yolov3_d53_320_273e_coco/yolov3_d53_320_273e_coco-421362b6.pth \
+    https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_x_8x8_300e_coco/yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth \
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py \
     https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth \
     --img-root ../dataset/cuhk_03/images_detected/  \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES='1' python demo/top_down_img_demo_with_mmdet_outhm.py \
 
 CUDA_VISIBLE_DEVICES='1' python demo/top_down_img_demo_with_mmdet_outhm.py \
     ../mmdetection/configs/yolox/yolox_x_8x8_300e_coco.py \
-    https://download.openmmlab.com/mmdetection/v2.0/yolo/yolov3_d53_320_273e_coco/yolov3_d53_320_273e_coco-421362b6.pth \
+    https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_x_8x8_300e_coco/yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth \
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py \
     https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth \
     --img-root ../dataset/cuhk_03/images_labeled/  \
